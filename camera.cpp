120c62e2f75fb38b283df4640185d8f672a2228a
turtlewizard73 <mate.laszlo703@gmail.com>
2024-12-11 11:35:35 +0000

    add debug print for camera infos

--- camera.cpp
+++ camera.cpp
@@ -11,6 +11,25 @@ int main()
   // Connect to device and start pipeline
   dai::Device device(pipeline);
 
+  // Print device name
+  std::cout << "Device name: " << device.getDeviceName() << std::endl;
+
+  // Bootloader version
+  if (device.getBootloaderVersion()) {
+    std::cout << "Bootloader version: " << device.getBootloaderVersion()->toString() << std::endl;
+  }
+
+  // Print out USB speed
+  std::cout << "Usb speed: " << device.getUsbSpeed() << std::endl;
+
+  // Connected cameras
+  auto cameras = device.getConnectedCameraFeatures();
+  std::cout << "Connected cameras: ";
+  for (const auto & cam : cameras) {
+    std::cout << cam << " ";
+  }
+  std::cout << std::endl;
+
   // Output queues will be used to get the grayscale frames from the outputs defined above
   while (true) {
     printf("Camera ready!\n");
